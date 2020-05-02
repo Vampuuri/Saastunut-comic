031f3f7f97c080fdf6bcb72ac4e54becf260b783
Essi Supponen <essi.supponen@tuni.fi>
2020-05-03 02:35:50 +0300

    DataService: add fetches for characters

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Character } from './data-interfaces/character';
+
 @Injectable()
 export class DataService {
 
@@ -8,5 +10,17 @@ export class DataService {
 
   getPages() {
     this.http.get('http://localhost:3000/pages').subscribe(res => {console.log(res)});
+  }
+
+  getMainCharacters(onSuccess: (res: any) => any, onError: (res: any) => any): void {
+    this.http.get<Character[]>('http://localhost:3000/characters/main').subscribe(res => {
+      onSuccess(res);
+    });
+  }
+
+  getSideCharacters(onSuccess: (res: any) => any, onError: (res: any) => any): void {
+    this.http.get<Character[]>('http://localhost:3000/characters/side').subscribe(res => {
+      onSuccess(res);
+    });
   }
 }
