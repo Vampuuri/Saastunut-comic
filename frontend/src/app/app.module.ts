a82ca34d9779230b985707ad44fc6715da9ffa45
Essi Supponen <essi.supponen@tuni.fi>
2020-05-03 00:38:15 +0300

    Add Frontpage component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/frontpage/frontpage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frontpage',
+  template: `
+    <h1>Frontpage</h1>
+  `,
+  styles: []
+})
+export class FrontpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
