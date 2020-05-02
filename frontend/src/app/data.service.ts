00695b22ba416394a65b952d569f0ebdbfca5e13
Essi Supponen <essi.supponen@tuni.fi>
2020-05-03 02:59:12 +0300

    DataService: add method for fetching active turn

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Character } from './data-interfaces/character';
+import { Turn } from './data-interfaces/turn';
 
 @Injectable()
 export class DataService {
@@ -20,6 +21,12 @@ export class DataService {
 
   getSideCharacters(onSuccess: (res: any) => any, onError: (res: any) => any): void {
     this.http.get<Character[]>('http://localhost:3000/characters/side').subscribe(res => {
+      onSuccess(res);
+    });
+  }
+
+  getActiveTurn(onSuccess: (res: any) => any, onError: (res: any) => any): void {
+    this.http.get<Turn>('http://localhost:3000/turns/active').subscribe(res => {
       onSuccess(res);
     });
   }
