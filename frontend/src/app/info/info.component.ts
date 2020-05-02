65d1bbb1cf1ddff823831cd2377e15a80b2997c6
Essi Supponen <essi.supponen@tuni.fi>
2020-05-03 01:32:14 +0300

    Add text to info

--- frontend/src/app/info/info.component.ts
+++ frontend/src/app/info/info.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info',
-  templateUrl: './info.component.html',
+  templateUrl: 'info.component.html',
   styles: ['']
 })
 export class InfoComponent implements OnInit {
+  rulesVisible: boolean = false;
 
   constructor() { }
 
+  toggleRules() {
+    this.rulesVisible = !this.rulesVisible;
+  }
+
   ngOnInit(): void {
+    console.log("info")
   }
 
 }
