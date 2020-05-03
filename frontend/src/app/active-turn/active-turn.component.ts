e8d2f96a2587634fe2ce3908b6e85563cb4b0445
Essi Supponen <essi.supponen@tuni.fi>
2020-05-03 03:10:32 +0300

    ActiveTurnComponent: fetch current active turn from backend

--- frontend/src/app/active-turn/active-turn.component.ts
+++ frontend/src/app/active-turn/active-turn.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+import { Turn } from '../data-interfaces/turn';
 
 @Component({
   selector: 'app-active-turn',
@@ -6,11 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./active-turn.component.css']
 })
 export class ActiveTurnComponent implements OnInit {
-  turn = {artist: 'Vampuuri', date: '2014.15.16'}
+  turn: Turn = {id: 0,
+    artist: '',
+    round: 0,
+    date: null,
+    done: false,
+    active: false,
+    skipped: false
+  }
 
-  constructor() { }
+  updateTurn(newTurn: Turn) {
+    this.turn = newTurn;
+  }
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getActiveTurn((res) => {this.updateTurn(res)}, () => {});
   }
 
 }
