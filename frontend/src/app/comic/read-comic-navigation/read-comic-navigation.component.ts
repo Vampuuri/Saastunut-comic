cbba185d296b8894c505dd995545160388c530b8
Essi Supponen <essi.supponen@tuni.fi>
2020-05-03 13:16:20 +0300

    ReadComicNavigationComponent: add working links as a list

--- frontend/src/app/comic/read-comic-navigation/read-comic-navigation.component.ts
+++ frontend/src/app/comic/read-comic-navigation/read-comic-navigation.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../../data.service';
+import { Page } from '../../data-interfaces/page';
+
 @Component({
   selector: 'app-read-comic-navigation',
   templateUrl: './read-comic-navigation.component.html',
   styleUrls: ['./read-comic-navigation.component.css']
 })
 export class ReadComicNavigationComponent implements OnInit {
+  pages: Page[] = []
 
-  constructor() { }
+  updatePages(newPages: Page[]) {
+    this.pages = newPages;
+  }
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getPages((res) => {this.updatePages(res)}, () => {});
   }
 
 }
