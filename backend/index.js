164f55ba37a084245e5d2bc87e32c986d7affd2f
Essi Supponen <essi.supponen@tuni.fi>
2020-05-17 15:09:36 +0300

    Backend: Replace all missing mappings

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.get('/characters/main', function(req,res) {
                 comicCharacter c
                 INNER JOIN artist a
                 ON c.artistId = a.id
-                WHERE c.mainCharacter = 1;`
+                WHERE c.mainCharacter = true;`
             con.query(sql, function (err, result) {
                 if (err) {
                     throw err;
@@ -57,7 +57,7 @@ app.get('/characters/side', function(req,res) {
                 comicCharacter c
                 INNER JOIN artist a
                 ON c.artistId = a.id
-                WHERE c.mainCharacter = 0;`
+                WHERE c.mainCharacter = false;`
             con.query(sql, function (err, result) {
                 if (err) {
                     throw err;
@@ -78,7 +78,8 @@ app.get('/pages', function(req,res) {
             sql = `SELECT p.*, a.username FROM
                 page p
                 INNER JOIN artist a
-                ON p.artistId = a.id;`
+                ON p.artistId = a.id
+                ORDER BY p.id;`
             con.query(sql, function (err, result) {
                 if (err) {
                     throw err;
@@ -123,33 +124,66 @@ app.get('/pages/:id([0-9]+)', function(req,res) {
     })
 });
 
-/**
-
 app.get('/artists', function(req,res) {
-    res.send(db.get('artists'));
+    con.getConnection(function(err) {
+        if (err) {
+            throw err;
+        } else {
+            sql = `SELECT * FROM artist`
+            con.query(sql, function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log(`artists fetched`)
+                    res.send(result);
+                }
+              });
+        }
+    })
 });
 
 app.get('/turns', function(req,res) {
-    res.send(db.get('turns'));
+    con.getConnection(function(err) {
+        if (err) {
+            throw err;
+        } else {
+            sql = `SELECT t.*, a.username
+                FROM turn t
+                INNER JOIN artist a
+                ON t.artistId = a.id`
+            con.query(sql, function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log(`turns fetched`)
+                    res.send(result);
+                }
+              });
+        }
+    })
 });
 
 app.get('/turns/active', function(req,res) {
-    var turnsArray = db.get('turns');
-    var turn = turnsArray[turnsArray.length - 1];
-
-    if (turn.active) {
-        res.send(turn);
-    } else {
-        res.status(404)
-        res.send({
-            code: 404,
-            message: 'Active turn not found',
-            description: 'Active turn was not found'
-        })
-    }
+    con.getConnection(function(err) {
+        if (err) {
+            throw err;
+        } else {
+            sql = `SELECT t.*, a.username
+                FROM turn t
+                INNER JOIN artist a
+                ON t.artistId = a.id
+                WHERE active = true`
+            con.query(sql, function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log(`active turn fetched`)
+                    res.send(result);
+                }
+              });
+        }
+    })
 });
-
-*/
 
 var server = app.listen(3000, function() {
     console.log('Server listening...')
