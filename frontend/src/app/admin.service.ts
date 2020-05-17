db22293841e622e4909bb80fab166e29e9f154c4
Essi Supponen <essi.supponen@tuni.fi>
2020-05-17 19:35:42 +0300

    Add post and put methods for pages

--- frontend/src/app/admin.service.ts
+++ frontend/src/app/admin.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Page } from './data-interfaces/page';
+
+@Injectable()
 export class AdminService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postNewPage(page: Page, artistId: number, onSuccess: (page: Page) => any, onError: () => any) {
+    this.http.post<Page>('http://localhost:3000/pages', {artistId: artistId, content: page.content, date: page.date})
+      .subscribe(res => {onSuccess(res)}, () => onError());
+  }
+
+  updatePage(page: Page, artistId: number, onSuccess: () => any, onError: () => any) {
+    this.http.put(`http://localhost:3000/pages/${page.id}`, {artistId: artistId, content: page.content, date: page.date})
+      .subscribe(() => onSuccess(), () => onError())
+  }
+
 }
